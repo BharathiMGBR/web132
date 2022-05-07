5f640c0981ecc09c433eb83c353eeb1bf47348c9
HP <HP@DESKTOP-PTJV6H8>
2022-05-07 16:45:03 +0530

    First Commit

---
+++ add/src/add/FirstProgram.java
@@ -0,0 +1,15 @@
+package add;
+
+public class FirstProgram {
+
+	public static void main(String[] args) {
+		 int a=4;
+		 int b=6;
+		 int c=a+b;
+		 System.out.println("the sum of two numbers is = "+c);
+		 
+		 
+
+	}
+
+}
