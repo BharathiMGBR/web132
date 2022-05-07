b3519ac095810e88474bcc4236f7b36764412d99
HP <HP@DESKTOP-PTJV6H8>
2022-05-07 17:05:00 +0530

    ADD

---
+++ add/src/add/ADD.java
@@ -0,0 +1,14 @@
+package add;
+
+public class ADD {
+	public static void main(String[] args) {
+	int a=4;
+	 int b=6;
+	 int c=a+b;
+	
+	 System.out.println("the sum of two numbers is = "+c);
+	 
+	 
+
+}
+}
